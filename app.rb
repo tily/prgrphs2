638dd54b4af05af956ff66a53da8a2d8ed9756f5
tily <tidnlyam@gmail.com>
2015-07-04 10:34:13 +0900

    tweak placeholder text

--- app.rb
+++ app.rb
@@ -96,4 +96,4 @@ __END__
       - else
         .post
           %form.form{method:'POST'}
-            %input.form-control{type:'text',name:'text',placeholder:'username or url ...'}
+            %input.form-control{type:'text',name:'text',placeholder:'Username or URL ...'}
